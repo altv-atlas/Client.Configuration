b020f1cc21881b4710016597c040fd7c9eac182d
Niels <7136117+TheDutchDev@users.noreply.github.com>
2023-11-26 21:17:55 +0100

    initial commit

---
+++ ConfigurationLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using AltV.Net.Client;
+
+namespace AltV.Atlas.Client.Configuration;
+
+public class ConfigurationLoader
+{
+    /// <summary>
+    /// Set of options to use when serializing/deserializing JSON objects
+    /// </summary>
+    public static readonly JsonSerializerOptions JsonSerializerOptions = new( )
+    {
+        PropertyNameCaseInsensitive = true,
+        IncludeFields = true,
+        NumberHandling = JsonNumberHandling.AllowReadingFromString
+    };
+    
+    public static T Load<T>( string filePath )
+    {
+        if( !Alt.FileExists( filePath ) )
+            throw new FileNotFoundException( "[ATLAS] Failed to find {File}", filePath );
+
+
+        var obj = JsonSerializer.Deserialize<T>( Alt.ReadFile( filePath ), JsonSerializerOptions );
+
+        if( obj is null )
+            throw new NullReferenceException( "[ATLAS] Failed to convert file to object" );
+
+        return obj;
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Client.Configuration
+Contains some helper methods for loading configuration (JSON) files
+
+## Documentation
+Please refer to our [official docs](https://altv-atlas.github.io/docs/index.html) for more information.
+
+## Class Diagram
+![Class Diagram](https://altv-atlas.github.io/docs/images/chat-module.png)
