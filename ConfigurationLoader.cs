6ee861d6c572946fc0d17ef0e41c6538c1e69662
Niels <7136117+TheDutchDev@users.noreply.github.com>
2023-12-03 10:27:47 +0100

    add missing xdocs

--- ConfigurationLoader.cs
+++ ConfigurationLoader.cs
@@ -4,6 +4,9 @@ using AltV.Net.Client;
 
 namespace AltV.Atlas.Client.Configuration;
 
+/// <summary>
+/// Helper class to load JSON configuration files via Alt.File API
+/// </summary>
 public class ConfigurationLoader
 {
     /// <summary>
@@ -16,6 +19,14 @@ public class ConfigurationLoader
         NumberHandling = JsonNumberHandling.AllowReadingFromString
     };
     
+    /// <summary>
+    /// Load a given JSON file with Alt.File API
+    /// </summary>
+    /// <param name="filePath">The JSON file to load</param>
+    /// <typeparam name="T">The type it should be cast to</typeparam>
+    /// <returns>The deserialized JSON object if it succeeded</returns>
+    /// <exception cref="FileNotFoundException">File could not be found</exception>
+    /// <exception cref="NullReferenceException">File could not be converted to the target type (T)</exception>
     public static T Load<T>( string filePath )
     {
         if( !Alt.FileExists( filePath ) )
